4a95f9ab352013fa89a54c636894c59d3e796455
Márcio S. Manske <mmanske@gmail.com>
2017-02-23 17:01:31 -0300

    Reservation methods

--- routes/contract.js
+++ routes/contract.js
@@ -7,9 +7,32 @@ var express = require('express');
 var router = express.Router();
 var config = require("../config/config");
 var BASE_SERVICE_URL = config.baseServiceUrlContract + "/contract";
+var RestService = require("../services/RestService");
+var restService = new RestService();
+
 
 
 var RouterBuilder = require("./RouterBuilder");
 new RouterBuilder().build(router, BASE_SERVICE_URL);
+
+router.post("/reservation", function(req, res) {
+    var serviceConfig = {url: BASE_SERVICE_URL+"/reservation", body: req.body, method: "post"};
+    restService.execute(serviceConfig, res);
+});
+
+router.put("/reservation", function(req, res) {
+    var serviceConfig = {url: BASE_SERVICE_URL+"/reservation", body: req.body, method: "put"};
+    restService.execute(serviceConfig, res);
+});
+
+router.get("/:contractId/:reservationId", function (req, res) {
+
+
+    var serviceConfig = {url: BASE_SERVICE_URL+"/reservation" + "/" + req.params.contractId + "/" + req.params.reservationId, body: null, method: "get"};
+
+    restService.execute(serviceConfig, res);
+
+});
+
 
 module.exports = router;
